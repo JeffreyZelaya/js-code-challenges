f96ef22e7ec95d17aa35d307e56f5f4e8a3d887e
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-02-12 23:43:04 -0700

    added sum function with for, while, and do while loops in bootstrap

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,2 +1,25 @@
-// ES6 code goes here
-console.log('es6 code goes here');
+const arrForSum = (arr) => {let sum = 0; for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+    console.log(sum)
+  }
+  
+  arrForSum([1,2,3,4])
+  
+  
+  const arrWhileSum = (arr) => {let sum = 0; let i = 0; while ( i < arr.length) {
+    sum += arr[i];  i++;
+  }
+    console.log(sum)
+  }
+  
+  arrWhileSum([1,2,3,4,5])
+  
+  
+  const arrDoWhileSum = (arr) => {let sum = 0; let i = 0; do {sum += arr[i]; i++
+  } while ( i < arr.length) {
+    console.log(sum)
+  }
+  }
+  
+  arrDoWhileSum([1,2,3,4,5,6])
